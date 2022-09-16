2f876d2df07a0b1f4b4b4ddbfac741dcf00ecee3
Luiz Zenha <lzenha@gmail.com>
2022-09-15 21:47:17 -0300

    Flyway migrations

---
+++ src/main/resources/db/migration/V1__assists.sql
@@ -0,0 +1,14 @@
+create table assistances
+(
+    id          bigint auto_increment
+        primary key,
+    description varchar(300) not null,
+    name        varchar(100) not null
+);
+
+INSERT INTO assistances (name, description) VALUES ('Troca de aparelho', 'Troca de aparelho decodificador de sinal');
+INSERT INTO assistances (name, description) VALUES ('Troca de cabo interno', 'Troca de cabo interno');
+INSERT INTO assistances (name, description) VALUES ('Troca de fiação interna', 'Substituição de fiação interna da residência');
+INSERT INTO assistances (name, description) VALUES ('Manutenção em fogão', 'Reparo sem necessidade de compra de peças');
+INSERT INTO assistances (name, description) VALUES ('Manutenção em geladeira', 'Reparo sem necessidade de compra de peças');
+INSERT INTO assistances (name, description) VALUES ('Manutenção em máquina de lavar', 'Reparo sem necessidade de compra de peças');
